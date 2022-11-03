f38c95d13d3492bcc94f1f886ee1d3ecc589189f
Ganiroy34 <109405679+Ganiroy34@users.noreply.github.com>
2022-11-03 20:36:22 +0530

    def gani(a):     g=list(str(a))     e=g.count('6')     if e>0:         for i in range(0,len(g)):             if g[i]=='6':                 g[i]='9'                 break         return g     elif e==0:         return g t=int(input()) g=gani(t) for j in g:     print(j,end="")

---
+++ Maximum_69_Number.py
@@ -0,0 +1,15 @@
+def gani(a):
+    g=list(str(a))
+    e=g.count('6')
+    if e>0:
+        for i in range(0,len(g)):
+            if g[i]=='6':
+                g[i]='9'
+                break
+        return g
+    elif e==0:
+        return g
+t=int(input())
+g=gani(t)
+for j in g:
+    print(j,end="")
