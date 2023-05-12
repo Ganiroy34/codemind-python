ea75711e2dc6eb98472e1b6c5e4056c5877b9b23
Ganiroy34 <109405679+Ganiroy34@users.noreply.github.com>
2023-05-12 14:36:57 +0530

    a=int(input()) s=0 f=0 if (a<0):     a=abs(a)     f=1 while(a>0):     g=a%10     s=g+s*10     a=a//10
    if(f==1):
        print(-1*s)
    else:print(s)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,19 +1,14 @@
-x=int(input())
+a=int(input())
 s=0
-r=x
-if x>0:
-    while x>0:
-        n=x%10
-        s=s*10+n
-        x=x//10
-else :
-    x=-x
-    while x>0:
-        
-        n=x%10
-        s=s*10+n
-        x=x//10
-if r>0:
-    print(s)
-else:
-    print(-s)
+f=0
+if (a<0):
+    a=abs(a)
+    f=1
+while(a>0):
+    g=a%10
+    s=g+s*10
+    a=a//10
+    
+if(f==1):
+    print(-1*s)
+else:print(s)    
